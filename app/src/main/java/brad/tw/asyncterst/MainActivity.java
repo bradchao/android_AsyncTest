efd0fab45bff2ae644b05be0f385f9ac6b5cb22c
Brad Chao <brad@brad.tw>
2016-09-20 10:22:04 +0800

    v7

--- app/src/main/java/brad/tw/asyncterst/MainActivity.java
+++ app/src/main/java/brad/tw/asyncterst/MainActivity.java
@@ -1,20 +1,33 @@
 package brad.tw.asyncterst;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     private MyTask mt1;
     private TextView mesg;
+    private ImageView img;
+    private Bitmap bmp;
+    private UIHandler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mesg = (TextView)findViewById(R.id.mesg);
+        img = (ImageView)findViewById(R.id.img);
+        handler = new UIHandler();
     }
     public void test1(View v){
         mt1 = new MyTask();
@@ -23,6 +36,31 @@ public class MainActivity extends AppCompatActivity {
     public void test2(View v){
         if (mt1 != null && !mt1.isCancelled()){
             mt1.cancel(true);
+        }
+    }
+    public void test3(View v){
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("http://www.androidcentral.com/sites/androidcentral.com/files/styles/w700/public/postimages/%5Buid%5D/podcast-ac-new.jpg?itok=tKuELSVP");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.connect();
+                    bmp = BitmapFactory.decodeStream(conn.getInputStream());
+                    handler.sendEmptyMessage(0);
+                }catch(Exception ee){
+                    Log.d("brad", ee.toString());
+                }
+            }
+        }.start();
+    }
+
+    private class UIHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            img.setImageBitmap(bmp);
+
         }
     }
 
